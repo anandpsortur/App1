65ef71813063dcb6a3fda938e31b3115868bd950
anandpsortur <anandpsortur@gmail.com>
2016-10-13 23:50:23 +0530

    Initial Commit

---
+++ TestingGitRepo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestingGitRepo
+//
+//  Created by Anand Sortur on 13/10/16.
+//  Copyright © 2016 Infinity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
